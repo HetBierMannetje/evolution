555cf05e4dfe50b879e86bda2ce01994c1279edd
HetBierMannetje <holgerbiegwork@gmail.com>
2025-06-14 20:55:59 +0200

    Update MuscleData.cs
    Hopefully adding snap to muscle functionality.

--- Assets/Scripts/Data/MuscleData.cs
+++ Assets/Scripts/Data/MuscleData.cs
@@ -1,68 +1,79 @@
+// Assets/Scripts/Data/MuscleData.cs
 using System;
 using Keiwando.JSON;
 
 [Serializable]
 public struct MuscleData: IJsonConvertible {
 
-    public readonly int id;
-    public readonly int startBoneID;
-    public readonly int endBoneID;
-
-    public readonly float strength;
-    public readonly bool canExpand;
-    public readonly string userId;
+	public int id;
+	public int startBoneID;
+	public int endBoneID;
+    public int startJointID; // Added for joint-to-joint muscle
+    public int endJointID;   // Added for joint-to-joint muscle
+    public bool isAttachedToJoints; // Added to differentiate connection type
+	public float strength;
+	public bool canExpand;
+	public string userId;
 
     public MuscleData(int id, int startBoneID, int endBoneID, float strength, bool canExpand, string userId) {
         this.id = id;
         this.startBoneID = startBoneID;
         this.endBoneID = endBoneID;
+        this.startJointID = -1; // Default invalid ID
+        this.endJointID = -1;   // Default invalid ID
+        this.isAttachedToJoints = false;
         this.strength = strength;
         this.canExpand = canExpand;
         this.userId = userId;
     }
 
-    #region Encode & Decode
-
-    private static class CodingKey {
-        public const string ID = "id";
-        public const string StartBoneID = "startBoneID";
-        public const string EndBoneID = "endBoneID";
-        public const string Strength = "strength";
-        public const string CanExpand = "canExpand";
-        // This is optional and only serialized if not ""
-        public const string UserID = "userID";
+    // New constructor for joint-to-joint muscles
+    public MuscleData(int id, int startJointID, int endJointID, float strength, bool canExpand, string userId, bool isAttachedToJoints) {
+        this.id = id;
+        this.startBoneID = -1; // Default invalid ID
+        this.endBoneID = -1;   // Default invalid ID
+        this.startJointID = startJointID;
+        this.endJointID = endJointID;
+        this.isAttachedToJoints = isAttachedToJoints;
+        this.strength = strength;
+        this.canExpand = canExpand;
+        this.userId = userId;
     }
 
-    public JObject Encode() {
-        var json = new JObject();
-        json[CodingKey.ID] = this.id;
-        json[CodingKey.StartBoneID] = this.startBoneID;
-        json[CodingKey.EndBoneID] = this.endBoneID;
-        json[CodingKey.Strength] = this.strength;
-        json[CodingKey.CanExpand] = this.canExpand;
-        if (!string.IsNullOrEmpty(this.userId)) {
-            json[CodingKey.UserID] = this.userId;
+	public JObject ToJson() {
+		JObject json = new JObject();
+		json.Add("id", id);
+        if (isAttachedToJoints) {
+            json.Add("startJointID", startJointID);
+            json.Add("endJointID", endJointID);
+            json.Add("isAttachedToJoints", isAttachedToJoints);
+        } else {
+            json.Add("startBoneID", startBoneID);
+		    json.Add("endBoneID", endBoneID);
         }
-        return json;
-    }
+		json.Add("strength", strength);
+		json.Add("canExpand", canExpand);
+		json.Add("userId", userId);
+		return json;
+	}
 
-    public static MuscleData Decode(string encoded) {
+	public static MuscleData FromJson(JObject json) {
+        int id = json.Get<JNumber>("id");
+        float strength = json.Get<JNumber>("strength");
+        bool canExpand = json.Get<JBoolean>("canExpand");
+        string userId = json.Get<JString>("userId");
 
-        var json = JObject.Parse(encoded);
-        return Decode(json);
-    }
+        // Handle legacy format without isAttachedToJoints
+        bool isAttachedToJoints = json.Get<JBoolean>("isAttachedToJoints", false);
 
-    public static MuscleData Decode(JObject json) {
-
-        int id = json[CodingKey.ID].ToInt();
-        int startID = json[CodingKey.StartBoneID].ToInt();
-        int endID = json[CodingKey.EndBoneID].ToInt();
-        float strength = json[CodingKey.Strength].ToInt();
-        bool canExpand = json[CodingKey.CanExpand].ToBool();
-        string userId = json.ContainsKey(CodingKey.UserID) ? json[CodingKey.UserID].ToString() : "";
-
-        return new MuscleData(id, startID, endID, strength, canExpand, userId);
-    }
-
-    #endregion
+        if (isAttachedToJoints) {
+            int startJointID = json.Get<JNumber>("startJointID");
+            int endJointID = json.Get<JNumber>("endJointID");
+            return new MuscleData(id, startJointID, endJointID, strength, canExpand, userId, true);
+        } else {
+            int startBoneID = json.Get<JNumber>("startBoneID");
+            int endBoneID = json.Get<JNumber>("endBoneID");
+            return new MuscleData(id, startBoneID, endBoneID, strength, canExpand, userId);
+        }
+	}
 }
